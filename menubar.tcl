d745c47a8c1858f57b90183fda04b44b8f5141a7
Manish Mani <manishpamu@hotmail.com>
2025-04-06 00:24:41 +0530

    Update menubar.tcl

--- menubar.tcl
+++ menubar.tcl
@@ -3,8 +3,58 @@ wm title . "Menubar"
 
 
 #set window aspect ratio
-#width			- 800
-#height			- 500
-#offset from left	- 500
-#offset from right	- 300
+#width  - 800
+#height  - 500
+#offset from left  - 500
+#offset from right  - 300
 wm geometry . 800x500+500+300
+
+#variables:::
+set menubar_tab_width 7
+
+
+#create a frame
+frame .menubar -relief groove \
+               -bd 2
+pack .menubar  -fill x
+
+#create menu buttons
+#FILE menu button
+menubutton .menubar.options_tab \
+                   -text "Options" \
+                   -justify left \
+                   -menu .menubar.options_tab.menu \
+                   -relief raised \
+                   -width $menubar_tab_width
+
+                   #initialize and configure the drop-down menu
+                   menu .menubar.options_tab.menu
+
+												#config a radio button
+												.menubar.options_tab.menu add radiobutton \
+																										  -label "Reconcile" \
+																											-accelerator "^R" \
+																											-command {bell}
+
+												#create a check button
+												.menubar.options_tab.menu add checkbutton \
+																											-label "Duplicate" \
+																											-accelerator "^D" \
+																											-command {bell}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
