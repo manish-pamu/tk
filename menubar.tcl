70181b6371ee430717846ea8e926fa5173225b54
Manish Mani <manishpamu@hotmail.com>
2025-04-06 00:10:21 +0530

    Create menubar.tcl

---
+++ menubar.tcl
@@ -0,0 +1,10 @@
+#set the window name
+wm title . "Menubar"
+
+
+#set window aspect ratio
+#width			- 800
+#height			- 500
+#offset from left	- 500
+#offset from right	- 300
+wm geometry . 800x500+500+300
